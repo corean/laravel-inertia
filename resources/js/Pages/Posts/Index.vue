fe21081c615429b1472904391991f7efc7653ccb
corean <corean@corean.biz>
2022-12-01 20:44:41 +0900

    - 페이징 처리

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Pagination from '@/Components/Pagination.vue';
+import moment from 'moment';
 
 defineProps({
   posts: {
@@ -19,6 +20,7 @@ defineProps({
         <th class="table-th">id</th>
         <th class="table-th">title</th>
         <th class="table-th">content</th>
+        <th class="table-th" style="width: 10em;">date</th>
       </tr>
       </thead>
       <tbody class="bg-white divide-y divide-gray-20 divide-solid">
@@ -26,6 +28,7 @@ defineProps({
         <td class="table-td">{{ post.id }}</td>
         <td class="table-td">{{ post.title }}</td>
         <td class="table-td">{{ post.content }}</td>
+        <td class="table-td">{{ moment(post.created_at).format('YYYY-MM-DD') }}</td>
       </tr>
       </tbody>
     </table>
