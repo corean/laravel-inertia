506fca6be4c846898f09eaae7fd82a055e911c26
corean <corean@corean.biz>
2022-12-01 19:55:50 +0900

    - tailwindcss autocomplete

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -8,20 +8,20 @@ defineProps({
 
 <template>
   <div class="max-w-5xl mx-auto">
-    <h1>Posts</h1>
+    <h1 class="text-2xl font-bold mb-4">Posts</h1>
     <table class="table-auto ">
       <thead>
       <tr>
-        <th class="px-6 py-3 bg-gray-50 text-left">id</th>
-        <th class="px-6 py-3 bg-gray-50 text-left">title</th>
-        <th class="px-6 py-3 bg-gray-50 text-left">content</th>
+        <th class="table-th">id</th>
+        <th class="table-th">title</th>
+        <th class="table-th">content</th>
       </tr>
       </thead>
       <tbody class="bg-white divide-y divide-gray-20 divide-solid">
       <tr v-for="post in posts" key="post.id">
-        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.id }}</td>
-        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.title }}</td>
-        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.content }}</td>
+        <td class="table-td">{{ post.id }}</td>
+        <td class="table-td">{{ post.title }}</td>
+        <td class="table-td">{{ post.content }}</td>
       </tr>
       </tbody>
     </table>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,11 @@
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
+        <!-- routes -->
         @routes
+        <!-- vite -->
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        <!-- inertiaHead -->
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
