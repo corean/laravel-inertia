1189c1f76952d883892835ef4869c25502a69c0f
corean <corean@corean.biz>
2022-12-01 23:00:06 +0900

    - 진행바 표시

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,8 @@ class PostController extends Controller
 {
     public function index(): \Inertia\Response
     {
+        sleep(3);
+
         $posts = PostResource::collection(Post::latest()->paginate(10));
         // $posts = Post::latest()->paginate(10);
         return Inertia::render('Posts/Index', [
--- resources/js/app.js
+++ resources/js/app.js
@@ -20,4 +20,18 @@ createInertiaApp({
     },
 });
 
-InertiaProgress.init({ color: '#4B5563' });
+// InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({
+    // The delay after which the progress bar will
+    // appear during navigation, in milliseconds.
+    delay: 250,
+
+    // The color of the progress bar.
+    color: '#29d',
+
+    // Whether to include the default NProgress styles.
+    includeCSS: true,
+
+    // Whether the NProgress spinner will be shown.
+    showSpinner: true,
+})
