6f1319b72987d50b47d80fc34fed4e0973f92f66
corean <corean@corean.biz>
2022-12-02 22:20:33 +0900

    - 메인 레이아웃 연결

--- resources/js/Pages/Posts/About.vue
+++ resources/js/Pages/Posts/About.vue
@@ -1,10 +1,13 @@
 <script setup>
-  import AppLayout from '@/Layouts/App.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head} from '@inertiajs/inertia-vue3';
+// import AppLayout from '@/Layouts/App.vue';
 </script>
 
 <template>
-  <AppLayout>
+  <Head title="About"/>
+  <AuthenticatedLayout>
     <h3>About Us</h3>
     <p>Text About </p>
-  </AppLayout>
+  </AuthenticatedLayout>
 </template>
--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,6 +1,8 @@
 <script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head} from '@inertiajs/inertia-vue3';
 import Pagination from '@/Components/Pagination.vue';
-import AppLayout from '@/Layouts/App.vue';
+// import AppLayout from '@/Layouts/App.vue';
 // import moment from 'moment';
 
 defineProps({
@@ -13,26 +15,36 @@ defineProps({
 </script>
 
 <template>
-  <AppLayout>
-    <table class="min-w-full divide-y divide-gray-200 border">
-      <thead>
-      <tr>
-        <th class="table-th">id</th>
-        <th class="table-th">title</th>
-        <th class="table-th">content</th>
-        <th class="table-th" style="width: 12em;">date</th>
-      </tr>
-      </thead>
-      <tbody class="bg-white divide-y divide-gray-20 divide-solid">
-      <tr v-for="post in posts.data" key="post.id">
-        <td class="table-td">{{ post.id }}</td>
-        <td class="table-td">{{ post.title }}</td>
-        <td class="table-td">{{ post.content }}</td>
-        <!--<td class="table-td">{{ moment(post.created_at).format('YYYY-MM-DD') }}</td>-->
-        <td class="table-td">{{ post.created_at }}</td>
-      </tr>
-      </tbody>
-    </table>
-    <pagination :data="posts.meta"/>
-  </AppLayout>
+  <Head title="Post List" />
+
+  <AuthenticatedLayout>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white py-8 px-6 sm:rounded-lg  shadow-sm ">
+          <table class="min-w-full divide-y divide-gray-100 border">
+            <thead>
+            <tr>
+              <th class="table-th">id</th>
+              <th class="table-th">title</th>
+              <th class="table-th">content</th>
+              <th class="table-th" style="width: 12em;">date</th>
+            </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-20 divide-solid">
+            <tr v-for="post in posts.data" key="post.id">
+              <td class="table-td">{{ post.id }}</td>
+              <td class="table-td">{{ post.title }}</td>
+              <td class="table-td">{{ post.content }}</td>
+              <!--<td class="table-td">{{ moment(post.created_at).format('YYYY-MM-DD') }}</td>-->
+              <td class="table-td">{{ post.created_at }}</td>
+            </tr>
+            </tbody>
+          </table>
+          <pagination :data="posts.meta"/>
+
+        </div>
+      </div>
+    </div>
+
+  </AuthenticatedLayout>
 </template>
